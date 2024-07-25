4633b7c42dc8f769ee552f55bba8b2e9bb02f173
Minh-Tam TRAN (Shodo) <minhtam-tran@ext.groupe-cyrus.fr>
2024-07-25 16:27:50 +0200

    🚀 feature         (nunit): Assert.That(list-with-single-null-item, Has.One.Items)

--- Tests.NUnit/NUnitApparently.cs
+++ Tests.NUnit/NUnitApparently.cs
@@ -13,4 +13,14 @@ public class NUnitApparently
 		// Assert
 		Assert.That(array, Has.One.Items);
 	}
+
+	[Test]
+	public void Accepts_The_Constraint__Has_One_Items__For_Single_Item_Collections_Even_When_Item_Is_Null()
+	{
+		// Arrange
+		var array = new int?[] { null };
+
+		// Assert
+		Assert.That(array, Has.One.Items);
+	}
 }
